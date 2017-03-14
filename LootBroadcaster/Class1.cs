5684ad9163013ed8f7eb14ffb0a48f671062b090
ariiZona <hublo11@hotmail.fr>
2017-03-14 10:34:04 -0400

    Skeleton. xd

--- LootBroadcaster/Class1.cs
+++ LootBroadcaster/Class1.cs
@@ -2,11 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
 
-namespace LootBroadcaster
+namespace TestPlugin
 {
-    public class Class1
+    [ApiVersion(2, 0)]
+    public class TestPlugin : TerrariaPlugin
     {
+        /// <summary>
+        /// Gets the author(s) of this plugin
+        /// </summary>
+        public override string Author
+        {
+            get { return "Anonymous"; }
+        }
+
+        /// <summary>
+        /// Gets the description of this plugin.
+        /// A short, one lined description that tells people what your plugin does.
+        /// </summary>
+        public override string Description
+        {
+            get { return "A sample test plugin"; }
+        }
+
+        /// <summary>
+        /// Gets the name of this plugin.
+        /// </summary>
+        public override string Name
+        {
+            get { return "Test Plugin"; }
+        }
+
+        /// <summary>
+        /// Gets the version of this plugin.
+        /// </summary>
+        public override Version Version
+        {
+            get { return new Version(1, 0, 0, 0); }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the TestPlugin class.
+        /// This is where you set the plugin's order and perfrom other constructor logic
+        /// </summary>
+        public TestPlugin(Main game) : base(game)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles plugin initialization. 
+        /// Fired when the server is started and the plugin is being loaded.
+        /// You may register hooks, perform loading procedures etc here.
+        /// </summary>
+        public override void Initialize()
+        {
+
+        }
+
+        /// <summary>
+        /// Handles plugin disposal logic.
+        /// *Supposed* to fire when the server shuts down.
+        /// You should deregister hooks and free all resources here.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Deregister hooks here
+            }
+            base.Dispose(disposing);
+        }
     }
 }
